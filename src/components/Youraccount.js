ea0e6b862b7414d4e2566dbb7e947b3b1f1ddeaf
EgorAvacado <rabotaga001@gmail.com>
2023-12-24 19:07:48 +0300

    Great changes

--- src/components/Youraccount.js
+++ src/components/Youraccount.js
@@ -1,22 +1,56 @@
-import React, { useState } from "react";
-import "../App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../App.css"; // Путь к вашему файлу со стилями
 
 const YourAccount = () => {
   const defaultAvatarImage = "/friends.jpg"; // Ссылка на картинку по умолчанию
+  const [gamesList, setGamesList] = useState([]);
+  const [selectedGames, setSelectedGames] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedGamesDisplay, setSelectedGamesDisplay] = useState([]);
 
-  const [gamesList, setGamesList] = useState(["Game 1", "Game 2", "Game 3"]);
+  useEffect(() => {
+    const fetchGames = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/api/games");
+        setGamesList(response.data);
+      } catch (error) {
+        console.error("Error fetching games", error);
+      }
+    };
+    fetchGames();
+  }, []);
 
-  const addGame = () => {
-    const newGame = prompt("Введите название новой игры:");
-    if (newGame) {
-      setGamesList([...gamesList, newGame]);
+  const handleAddGame = () => {
+    setShowModal(true);
+  };
+
+  const handleGameSelection = (gameId) => {
+    const isSelected = selectedGames.includes(gameId);
+    if (isSelected) {
+      setSelectedGames(selectedGames.filter((id) => id !== gameId));
+    } else {
+      setSelectedGames([...selectedGames, gameId]);
     }
   };
 
-  // Функция для удаления игры из списка
-  const deleteGame = (index) => {
-    const updatedGames = gamesList.filter((_, i) => i !== index);
-    setGamesList(updatedGames);
+  const handleSaveGames = async () => {
+    try {
+      await axios.post("http://localhost:3001/api/user/games/add", {
+        userId: "1",
+        games: selectedGames,
+      });
+
+      const updatedSelectedGames = gamesList.filter((game) =>
+        selectedGames.includes(game.id)
+      );
+      console.log(updatedSelectedGames); // Добавьте эту строку для проверки данных
+      setSelectedGamesDisplay(updatedSelectedGames);
+
+      setShowModal(false);
+    } catch (error) {
+      console.error("Error saving games to user", error);
+    }
   };
 
   return (
@@ -33,31 +67,59 @@ const YourAccount = () => {
           <div className="user-info-acc">
             <h1 className="welcome-text-acc">Welcome, Акула!</h1>
             <p>
-              Меня зовут AI-Gamer. Я являюсь виртуальным игровым ассистентом,
-              созданным для помощи и развлечения игроков. Мои основные
-              характеристики и возможности включают: 1. Игровой опыт: Я обладаю
-              большим опытом в различных жанрах игр, включая экшен, стратегии,
-              RPG и многие другие. Я могу подсказать игровые секреты, стратегии,
-              а также помочь в решении сложных задач и головоломок. 2.
-              Мультиплатформенность: Я знаком со многими популярными игровыми
-              платформами, включая ПК, консоли и мобильные устройства. Я могу
-              предоставить информацию о различных играх на этих платформах. 3.
-              Обновления и новости: Я всегда следу я могу
+              Привет, я Разрушитель! Я страстный геймер и любитель видеоигр. Мои
+              пальцы ловко управляют геймпадом или клавиатурой, а мои глаза
+              всегда сосредоточены на экране, где разворачиваются невероятные
+              приключения и сражения. Я пристрастился к широкому спектру игровых
+              жанров, от захватывающих экшенов и ролевых игр до интригующих
+              головоломок и соревновательных многопользовательских сражений.
+              Виртуальные миры стали моим вторым домом, а игровые персонажи -
+              верными спутниками. Когда я вступаю в игровой мир, я
+              сосредотачиваюсь на достижении высоких результатов и постоянном
+              развитии своего навыка игры. Я стремлюсь к тому, чтобы стать
+              лучшим в своей команде или соревновании и покорить самые сложные
+              испытания, с которыми сталкиваются геймеры.
             </p>
           </div>
         </div>
         <div className="games-acc">
-          {/* Добавляем кнопки "Добавить игру" и "Удалить игру" */}
           <div className="game-buttons">
-            <button onClick={addGame}>Добавить игру</button>
+            <button onClick={handleAddGame}>Добавить игру</button>
           </div>
-          <h3>Games played:</h3>
-          <ul className="game-list-acc">
-            {gamesList.map((game, index) => (
-              <li key={index}>
-                {game}
-                <button onClick={() => deleteGame(index)}>Удалить</button>
-              </li>
+          {showModal && (
+            <div className="modal">
+              <div className="modal-content">
+                <span className="close" onClick={() => setShowModal(false)}>
+                  &times;
+                </span>
+                <h3>Выберите игры:</h3>
+                <ul className="game-list">
+                  {gamesList.map((game) => (
+                    <li key={game.id}>
+                      <label>
+                        <input
+                          type="checkbox"
+                          value={game.id}
+                          onChange={() => handleGameSelection(game.id)}
+                          checked={selectedGames.includes(game.id)}
+                        />
+                        {game.title}
+                      </label>
+                    </li>
+                  ))}
+                </ul>
+                <button className="save-btn" onClick={handleSaveGames}>
+                  Сохранить выбранные игры
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="selected-games">
+          <h3>Выбранные игры:</h3>
+          <ul>
+            {selectedGamesDisplay.map((game) => (
+              <li key={game.id}>{game.title}</li>
             ))}
           </ul>
         </div>
